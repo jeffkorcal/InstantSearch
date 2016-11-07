7bc65eb519a60a12022c991790c273168871c891
Jeff Korcal <jkorcal@gmail.com>
2016-11-06 22:04:27 -0800

    code cleanup

--- scripts/updateIndex.js
+++ scripts/updateIndex.js
@@ -1,10 +1,11 @@
+// This script will update data in your index without replacing pre-existing content
+
 var algoliasearch = require('algoliasearch');
 var data = require('../resources/dataset/restaurants_list.json');
 
 var client = algoliasearch(process.env.APPLICATION_ID, process.env.ADMIN_API_KEY);
 var index = client.initIndex('Place2Eat');
 
-//Updates data without replacing existing content
 function partialUpdate (arr) {
 
   arr.forEach(function updateEach(obj) {
